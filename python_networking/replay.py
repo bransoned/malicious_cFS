434b0631ca2b375ebd4d1ee3ec8bce3729f07f1a
b-ranson <bransonmbailey04@gmail.com>
2024-07-01 07:34:53 -0600

    any work i did last week committing

--- python_networking/replay.py
+++ python_networking/replay.py
@@ -5,5 +5,6 @@ command = [0x18, 0x05, 0xC0, 0x00, 0x00, 0x01, 0x00, 0x23]
 byte_message = bytes(command)
 opened_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-while True:
+for i in range(50):
     opened_socket.sendto(byte_message, ("127.0.0.1", 1234))
+    print(byte_message[0])
